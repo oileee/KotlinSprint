d0a4082cb6f6701524a8d0c35c568812b9729afe
oileee <oilee@yandex.ru>
2024-09-21 14:03:44 +0300

    KS-1-2 print current data

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_1
+
+fun main() {
+    var numberOfOrders: Int = 75
+
+    val thankingForPurchaseText: String = "Спасибо за покупку!"
+
+    println(numberOfOrders)
+
+    println(thankingForPurchaseText)
+
+    var numberOfEmployees = 2000
+
+//    println(numberOfEmployees)
+
+    numberOfEmployees = 1999
+
+    println(numberOfEmployees)
+
+}
