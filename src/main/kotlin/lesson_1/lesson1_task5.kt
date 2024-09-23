fc9e69869db03880e94bff371317028b2f014b68
oileee <oilee@yandex.ru>
2024-09-23 11:18:05 +0300

    KS-1-5 assign constants

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,16 +1,18 @@
 package org.example.lesson_1
 
+const val SECONDS_IN_HOUR = 3600
+
+const val SECONDS_IN_MINUTE = 60
+
 fun main() {
     val secondsInSpace = 6480
 
-    val minutesInSpace = secondsInSpace / 60
+    val hours = secondsInSpace / SECONDS_IN_HOUR
 
-    val hoursInSpace = secondsInSpace / 3600
+    val minutes = secondsInSpace % SECONDS_IN_HOUR / SECONDS_IN_MINUTE
 
-    val seconds = secondsInSpace % 3600 % 60
+    val seconds = secondsInSpace % SECONDS_IN_HOUR % SECONDS_IN_MINUTE
 
-    val minutes = secondsInSpace % 3600 / 60
-
-    println(String.format("%02d:%02d:%02d", hoursInSpace, minutes, seconds))
+    println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
 
 }
