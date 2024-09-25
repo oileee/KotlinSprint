bc22b4fbcc31e26397785ab01aab70ca27e791bf
oileee <oilee@yandex.ru>
2024-09-25 11:57:45 +0300

    KS-2-5 refactor initializing

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -1,16 +1,14 @@
 package org.example.lesson_2
 
-//S = P*(1+ i)^n
-
 import kotlin.math.pow
 import kotlin.math.roundToInt
 
 fun main() {
     var initialDepositAmount = 70_000
-    var depositRate = 0.167
+    var depositRate = 16.7
     var depositTerm = 20
 
-    var finalDepositAmount = ((initialDepositAmount * (1 + depositRate).pow(depositTerm) * 1000).roundToInt()) / 1000.0
+    var finalDepositAmount = ((initialDepositAmount * (1 + depositRate / 100).pow(depositTerm) * 1000).roundToInt()) / 1000.0
 
     println(finalDepositAmount)
 
