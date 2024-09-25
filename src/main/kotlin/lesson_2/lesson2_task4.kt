6001a0fff05a2226ae2f2ba09b7e32ec055bc1fb
oileee <oilee@yandex.ru>
2024-09-25 11:44:16 +0300

    KS-2-4 remove magic numbers

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,15 +1,15 @@
 package org.example.lesson_2
 
 fun main() {
+    var buffPercent = 0.2
+
     var crystalOre = 7
     var ironOre = 11
 
-    var bonusCrystalOre = (crystalOre * 0.2).toInt()
-
-    var bonusIronOre = (ironOre * 0.2).toInt()
+    var bonusCrystalOre = (crystalOre * buffPercent).toInt()
+    var bonusIronOre = (ironOre * buffPercent).toInt()
 
     println("Бонусная кристаллическая руда: $bonusCrystalOre")
-
     println("Бонусная железная руда: $bonusIronOre")
 
 }
