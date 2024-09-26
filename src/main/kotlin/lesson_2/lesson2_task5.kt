4d02efe14c728a39c959fa99ca567d4d07b6d26d
oileee <oilee@yandex.ru>
2024-09-26 09:06:47 +0300

    KS-2-5 refactor output

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -1,15 +1,13 @@
 package org.example.lesson_2
 
 import kotlin.math.pow
-import kotlin.math.roundToInt
 
 fun main() {
     var initialDepositAmount = 70_000
     var depositRate = 16.7
     var depositTerm = 20
 
-    var finalDepositAmount = ((initialDepositAmount * (1 + depositRate / 100).pow(depositTerm) * 1000).roundToInt()) / 1000.0
+    var finalDepositAmount = initialDepositAmount * (1 + depositRate / 100).pow(depositTerm)
 
-    println(finalDepositAmount)
-
+    println("%.3f".format(finalDepositAmount))
 }
