8307a3be587cbbeec2a3f11486ac31ad641f9584
oileee <oilee@yandex.ru>
2024-09-28 01:24:38 +0300

    KS-4-2 correct code style

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -1,17 +1,23 @@
 package org.example.lesson_4
 
+const val MIN_WEIGHT_OF_AVERAGE_CATEGORY_KILOS = 35
+const val MAX_WEIGHT_OF_AVERAGE_CATEGORY_KILOS = 100
+const val MAX_VOLUME_OF_AVERAGE_CATEGORY_LITRES = 100
+
 fun main() {
-    val minWeightOfAverage = 35
-    val maxWeightOfAverageCategory = 100
-    val maxVolumeOfAverageCategory = 100
+    var weightOfCargoKilos = 20
+    var volumeOfCargoLitres = 80
 
-    var weightOfCargo = 20
-    var volumeOfCargo = 80
+    println("Груз с весом $weightOfCargoKilos кг и объемом $volumeOfCargoLitres л соответствует категории 'Average': " +
+            "${(weightOfCargoKilos > MIN_WEIGHT_OF_AVERAGE_CATEGORY_KILOS) 
+                    && (weightOfCargoKilos <= MAX_WEIGHT_OF_AVERAGE_CATEGORY_KILOS) 
+                    && (volumeOfCargoLitres < MAX_VOLUME_OF_AVERAGE_CATEGORY_LITRES)}")
 
-    println("Груз с весом $weightOfCargo кг и объемом $volumeOfCargo л соответствует категории 'Average': ${(weightOfCargo > minWeightOfAverage) && (weightOfCargo <= maxWeightOfAverageCategory) && (volumeOfCargo < maxVolumeOfAverageCategory)}")
+    weightOfCargoKilos = 50
+    volumeOfCargoLitres = 100
 
-    weightOfCargo = 50
-    volumeOfCargo = 100
-
-    println("Груз с весом $weightOfCargo кг и объемом $volumeOfCargo л соответствует категории 'Average': ${(weightOfCargo > minWeightOfAverage) && (weightOfCargo <= maxWeightOfAverageCategory) && (volumeOfCargo < maxVolumeOfAverageCategory)}")
+    println("Груз с весом $weightOfCargoKilos кг и объемом $volumeOfCargoLitres л соответствует категории 'Average': " +
+            "${(weightOfCargoKilos > MIN_WEIGHT_OF_AVERAGE_CATEGORY_KILOS) 
+                    && (weightOfCargoKilos <= MAX_WEIGHT_OF_AVERAGE_CATEGORY_KILOS) 
+                    && (volumeOfCargoLitres < MAX_VOLUME_OF_AVERAGE_CATEGORY_LITRES)}")
 }
