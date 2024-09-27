c0f90c8b479213f8ee4484d72d697076ee4aad1b
oileee <oilee@yandex.ru>
2024-09-28 02:06:11 +0300

    KS-4-4 refactor definition of even day

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -3,10 +3,15 @@ package org.example.lesson_4
 fun main() {
     var dayOfTraining = 5
 
-    println("Упражнения для рук: ${(dayOfTraining % 2) == 1}\nУпражнения для ног: ${(dayOfTraining % 2) == 0}\nУпражнения для спины: ${(dayOfTraining % 2) == 0}\nУпражнения для пресса: ${(dayOfTraining % 2) == 1}\n")
+    var isEvenDay = (dayOfTraining % 2) == 0
+
+    println("Упражнения для рук: ${!isEvenDay}\nУпражнения для ног: $isEvenDay\n" +
+            "Упражнения для спины: $isEvenDay\nУпражнения для пресса: ${!isEvenDay}\n")
 
     dayOfTraining = 6
 
-    println("Упражнения для рук: ${(dayOfTraining % 2) == 1}\nУпражнения для ног: ${(dayOfTraining % 2) == 0}\nУпражнения для спины: ${(dayOfTraining % 2) == 0}\nУпражнения для пресса: ${(dayOfTraining % 2) == 1}\n")
+    isEvenDay = (dayOfTraining % 2) == 0
 
+    println("Упражнения для рук: ${!isEvenDay}\nУпражнения для ног: $isEvenDay\n" +
+            "Упражнения для спины: $isEvenDay\nУпражнения для пресса: ${!isEvenDay}\n")
 }
