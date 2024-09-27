e07e799d803248d38b586018ad626247afa6c0bb
oileee <oilee@yandex.ru>
2024-09-28 02:33:13 +0300

    KS-4-5 change names of variables

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -5,14 +5,16 @@ const val RECOMMENDED_CREW_SIZE = 70
 const val MIN_NUMBER_OF_PROVISION_BOXES = 50
 
 fun main() {
-    val hullIsDamaged = readln().toBoolean()
+    val isHullDamaged = readln().toBoolean()
     val numberOfCrewMembers = readln().toInt()
     val numberOfProvisionBoxes = readln().toInt()
     val weatherIsGood = readln().toBoolean()
 
-    val conditionsAreMet1: Boolean = (!hullIsDamaged) && (numberOfCrewMembers in (MIN_CREW_SIZE..RECOMMENDED_CREW_SIZE)) && (numberOfProvisionBoxes > MIN_NUMBER_OF_PROVISION_BOXES)
-    val conditionsAreMet2: Boolean = (numberOfCrewMembers == RECOMMENDED_CREW_SIZE) && (weatherIsGood) && (numberOfProvisionBoxes >= MIN_NUMBER_OF_PROVISION_BOXES)
+    val isConditionMet1: Boolean = (!isHullDamaged) &&
+            (numberOfCrewMembers in (MIN_CREW_SIZE..RECOMMENDED_CREW_SIZE)) &&
+            (numberOfProvisionBoxes > MIN_NUMBER_OF_PROVISION_BOXES)
+    val isConditionMet2: Boolean = (numberOfCrewMembers == RECOMMENDED_CREW_SIZE) &&
+            (weatherIsGood) && (numberOfProvisionBoxes >= MIN_NUMBER_OF_PROVISION_BOXES)
 
-    println("Может ли корабль отправиться в плавание? ${conditionsAreMet1 || conditionsAreMet2}")
-
+    println("Может ли корабль отправиться в плавание? ${isConditionMet1 || isConditionMet2}")
 }
