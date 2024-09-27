56293e62a8351d72f0b49c1244ccb1cd601573a8
oileee <oilee@yandex.ru>
2024-09-28 01:40:29 +0300

    KS-4-3 correct code style

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,15 +1,18 @@
 package org.example.lesson_4
 
-const val SUNNY_WEATHER = true
-const val OPEN_TENT = true
+const val WEATHER_IS_SUNNY = true
+const val AWNING_IS_OPEN = true
 const val GOOD_HUMIDITY = 20
 const val BAD_SEASON = "зима"
 
 fun main() {
-    var weatherIsSunny: Boolean = true
-    var tentIsOpen: Boolean = true
+    var isSunnyWeather: Boolean = true
+    var isAwningOpen: Boolean = true
     var currentHumidity: Int = 20
     var currentSeason: String = "зима"
 
-    println("Благоприятные ли условия сейчас для роста бобовых? ${(weatherIsSunny == SUNNY_WEATHER) && (tentIsOpen == OPEN_TENT) && (currentHumidity == GOOD_HUMIDITY) && (currentSeason != BAD_SEASON)}")
+    println("Благоприятные ли условия сейчас для роста бобовых? ${(isSunnyWeather == WEATHER_IS_SUNNY) 
+            && (isAwningOpen == AWNING_IS_OPEN) 
+            && (currentHumidity == GOOD_HUMIDITY) 
+            && (currentSeason != BAD_SEASON)}")
 }
